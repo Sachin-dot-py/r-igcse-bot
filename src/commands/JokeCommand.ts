23fb952dc8b1f41ead21228f294b5683c6f093aa
11xdeveloper <neighborhood10xdeveloper@gmail.com>
2024-02-07 23:17:30 +0500

    feat: Added the /joke command (random joke)

---
+++ src/commands/JokeCommand.ts
@@ -0,0 +1,13 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import BaseCommand from '../utils/Structure/BaseCommand';
+import { getJoke } from '../utils/jokes';
+
+export default class TestCommand extends BaseCommand {
+    constructor() {
+        super(new SlashCommandBuilder().setName('joke'));
+    }
+
+    async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.reply(getJoke());
+    }
+}
