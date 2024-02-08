412b97a6470330fdc976b818814d5545efaf29fc
11xdeveloper <neighborhood10xdeveloper@gmail.com>
2024-02-08 14:50:16 +0500

    feat: Updated joke API & added categories to command

--- src/commands/JokeCommand.ts
+++ src/commands/JokeCommand.ts
@@ -1,17 +1,35 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import BaseCommand from '../registry/Structure/BaseCommand';
-import { getJoke } from '../utils/jokes';
+import { getJoke, type JokeCategory } from '../utils/joke';
 
 export default class JokeCommand extends BaseCommand {
     constructor() {
         super(
             new SlashCommandBuilder()
                 .setName('joke')
-                .setDescription('Get a random joke'),
+                .setDescription('Get a random joke')
+                .addStringOption((option) =>
+                    option
+                        .setName('category')
+                        .setDescription('specify category')
+                        .addChoices({
+                            name: 'programming',
+                            value: 'Programming',
+                        }),
+                ),
         );
     }
 
     async execute(interaction: ChatInputCommandInteraction) {
-        await interaction.reply(getJoke());
+        const category = interaction.options.getString('subreddit', false);
+        const categories: JokeCategory[] = category
+            ? [category as JokeCategory]
+            : ['Misc', 'Dark', 'Pun'];
+
+        await interaction.deferReply();
+
+        const joke = await getJoke(categories);
+
+        await interaction.followUp(joke);
     }
 }
---
+++ src/utils/joke.ts
@@ -0,0 +1,40 @@
+type APIResponse = {
+    error: false;
+    category: JokeCategory;
+    flags: {
+        nsfw: false;
+        religious: false;
+        political: false;
+        racist: false;
+        sexist: false;
+        explicit: false;
+    };
+    id: number;
+    safe: true;
+    lang: 'en';
+} & (
+    | { type: 'single'; joke: string }
+    | { type: 'twopart'; setup: string; delivery: string }
+);
+
+export type JokeCategory =
+    | 'Programming'
+    | 'Misc'
+    | 'Dark'
+    | 'Pun'
+    | 'Spooky'
+    | 'Christmas';
+
+export const getJoke = async (
+    categories: JokeCategory[],
+    amount: number = 1,
+) => {
+    const res = await fetch(
+        `https://v2.jokeapi.dev/joke/${categories.join(',')}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=${amount}`,
+    );
+    const data: APIResponse = await res.json();
+
+    return data.type === 'single'
+        ? data.joke
+        : `${data.setup} ${data.delivery}`;
+};
