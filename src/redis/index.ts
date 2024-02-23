767255e5b0b3e4e320ba9047c68e0499ee6b4c7d
chirag350 <me@chirag.dev>
2024-02-23 05:49:17 +0530

    Add redis schemas

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,7 +1,18 @@
-import { createClient } from 'redis';
+import { createClient, type RedisClientType } from 'redis';
+import { UserRepo } from './schemas/User';
+import { QuestionRRepo } from './schemas/Question';
+import { SessionRepo } from './schemas/Session'
+import { StickyMessageRepo } from './schemas/StickyMessage';
+import { ViewRepo } from './schemas/View'
 
-const redis = createClient({ url: process.env.REDIS_URL });
+export const redis = createClient({ url: process.env.REDIS_URL });
 
 redis.on('error', (err) => console.log('Redis Client Error', err));
 
 await redis.connect();
+
+export const User = new UserRepo(redis as RedisClientType);
+export const QuestionR = new QuestionRRepo(redis as RedisClientType);
+export const Session = new SessionRepo(redis as RedisClientType);
+export const StickyMessage = new StickyMessageRepo(redis as RedisClientType);
+export const View = new ViewRepo(redis as RedisClientType);
