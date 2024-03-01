cec4417be86e25f30c74e328eb14466643eb9c0f
11xdeveloper <neighborhood10xdeveloper@gmail.com>
2024-03-01 22:13:36 +0500

    feat: Added sticky message redis schema & repository (100th Commit)

--- src/redis/schemas/StickyMessage.ts
+++ src/redis/schemas/StickyMessage.ts
@@ -1,20 +1,45 @@
-// import { Repository, Schema, type Entity } from "redis-om";
-// import { redis } from "..";
+import {
+	Repository,
+	Schema,
+	type Entity,
+	type RedisConnection,
+} from "redis-om";
+import { redis } from "..";
 
-// interface ICachedStickyMessage extends Entity {
-// 	channelId: string;
-// 	messageId: string;
-// 	embedTitles: string[];
-// 	enabled: boolean;
-// }
+interface ICachedStickyMessage extends Entity {
+	channelId: string;
+	messageId: string;
+	embed: string;
+	stickTime: string;
+	unstickTime: string;
+	enabled: boolean;
+}
 
-// const schema = new Schema("StickyMessage", {
-// 	channelId: { type: "string" },
-// 	messageId: { type: "string" },
-// 	// embed: { type: "" },
-// 	stickTime: { type: "string" },
-// 	unstickTime: { type: "string" },
-// 	enabled: { type: "boolean" },
-// });
+const schema = new Schema("StickyMessage", {
+	channelId: { type: "string" },
+	messageId: { type: "string" },
+	embed: { type: "string" },
+	stickTime: { type: "string" },
+	unstickTime: { type: "string" },
+	enabled: { type: "boolean" },
+});
 
-// export const StickyMessageCache = new Repository(schema, redis);
+class StickyMessageRepository extends Repository {
+	constructor(clientOrConnection: RedisConnection) {
+		super(schema, clientOrConnection);
+		this.createIndex();
+	}
+
+	async get(sessionId: string) {
+		return (await this.fetch(sessionId)) as ICachedStickyMessage;
+	}
+
+	async set(messageId: string, stickyMessageData: ICachedStickyMessage) {
+		return (await this.save(
+			messageId,
+			stickyMessageData,
+		)) as ICachedStickyMessage;
+	}
+}
+
+export const StickyMessageCache = new StickyMessageRepository(redis);
