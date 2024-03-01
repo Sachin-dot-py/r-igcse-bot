11fc1a4dd2d9587444b2ace2368dc472e10afd8a
11xdeveloper <neighborhood10xdeveloper@gmail.com>
2024-03-01 21:54:10 +0500

    feat: Finished /message send (modal)

--- src/commands/moderation/Message.ts
+++ src/commands/moderation/Message.ts
@@ -1,8 +1,18 @@
+import { logger } from "@/index";
 import type { DiscordClient } from "@/registry/DiscordClient";
 import BaseCommand, {
 	type DiscordChatInputCommandInteraction,
 } from "@/registry/Structure/BaseCommand";
-import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import {
+	ActionRowBuilder,
+	ComponentType,
+	ModalBuilder,
+	PermissionFlagsBits,
+	SlashCommandBuilder,
+	TextChannel,
+	TextInputBuilder,
+	TextInputStyle,
+} from "discord.js";
 
 export default class KickCommand extends BaseCommand {
 	constructor() {
@@ -41,8 +51,58 @@ export default class KickCommand extends BaseCommand {
 		if (!interaction.guild) return;
 
 		if (interaction.options.getSubcommand() === "send") {
+			if (!interaction.channel) return;
+
 			const channel =
 				interaction.options.getChannel("channel", false) || interaction.channel;
+
+			if (!(channel instanceof TextChannel)) return;
+
+			const replyMessageId = new TextInputBuilder()
+				.setCustomId("replyMessageId")
+				.setLabel("Reply Message Id")
+				.setPlaceholder("ID of the message you want to reply to")
+				.setRequired(false)
+				.setStyle(TextInputStyle.Short);
+
+			const messageContent = new TextInputBuilder()
+				.setCustomId("messageContent")
+				.setLabel("Message Content")
+				.setPlaceholder("The body of the message you wish to send")
+				.setRequired(true)
+				.setStyle(TextInputStyle.Paragraph);
+
+			const row1 = new ActionRowBuilder<TextInputBuilder>().addComponents(
+				replyMessageId,
+			);
+
+			const row2 = new ActionRowBuilder<TextInputBuilder>().addComponents(
+				messageContent,
+			);
+
+			const modal = new ModalBuilder()
+				.setTitle("Send a message!")
+				.setCustomId("sendMessage")
+				.addComponents(row1, row2);
+
+			await interaction.showModal(modal);
+
+			interaction
+				.awaitModalSubmit({
+					filter: (i) =>
+						i.customId === "sendMessage" && i.user.id === interaction.user.id,
+
+					time: 24000000,
+				})
+				.then(async (i) => {
+					await channel.send({
+						content: i.fields.getTextInputValue("messageContent"),
+						reply: {
+							messageReference: i.fields.getTextInputValue("replyMessageId"),
+						},
+					});
+				})
+				.catch(logger.error);
 		} else if (interaction.options.getSubcommand() === "edit") {
 			const messageId = interaction.options.getChannel("messageId", true);
 		}
