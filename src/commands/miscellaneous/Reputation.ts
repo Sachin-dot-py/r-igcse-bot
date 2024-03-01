eb77bb42d4e80a279e034a227c32d5b89d09fb30
11xdeveloper <neighborhood10xdeveloper@gmail.com>
2024-03-01 19:49:48 +0500

    feat: Added /rep (view users rep)

---
+++ src/commands/miscellaneous/Reputation.ts
@@ -0,0 +1,43 @@
+import { SlashCommandBuilder } from "discord.js";
+import BaseCommand, {
+	type DiscordChatInputCommandInteraction,
+} from "../../registry/Structure/BaseCommand";
+import type { DiscordClient } from "@/registry/DiscordClient";
+import { Reputation } from "@/mongo";
+
+export default class ReputationCommand extends BaseCommand {
+	constructor() {
+		super(
+			new SlashCommandBuilder()
+				.setName("rep")
+				.setDescription("View someone's current rep")
+				.addUserOption((option) =>
+					option
+						.setName("user")
+						.setDescription("The user to view the rep of")
+						.setRequired(false),
+				)
+				.setDMPermission(false),
+		);
+	}
+
+	async execute(
+		client: DiscordClient,
+		interaction: DiscordChatInputCommandInteraction,
+	) {
+		if (!interaction.guild) return;
+
+		const user = interaction.options.getUser("user", false) ?? interaction.user;
+
+		const res = await Reputation.findOne({
+			guildId: interaction.guild.id,
+			userId: user.id,
+		}).exec();
+
+		const rep = res?.rep || 0;
+
+		await interaction.reply({
+			content: `${user.username} has ${rep} rep`,
+		});
+	}
+}
