9ea5dc4b9e91d2bad55c210486ea5a73fd4e6920
11xdeveloper <neighborhood10xdeveloper@gmail.com>
2024-03-13 14:41:20 +0500

    chore: remove unused imports

--- src/events/Error.ts
+++ src/events/Error.ts
@@ -1,8 +1,6 @@
-import { Events, MessageReaction, User } from "discord.js";
-import BaseEvent from "../registry/Structure/BaseEvent";
+import { Events } from "discord.js";
 import type { DiscordClient } from "../registry/DiscordClient";
-import { handleVote } from "./voteHandler";
-import { ReactionRole } from "@/mongo";
+import BaseEvent from "../registry/Structure/BaseEvent";
 
 export default class InteractionCreateEvent extends BaseEvent {
 	constructor() {
