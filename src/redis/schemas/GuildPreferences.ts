a06679b4ab9eb71eb632f5905150958d8480aa1f
11xdeveloper <neighborhood10xdeveloper@gmail.com>
2024-03-15 17:13:06 +0500

    feat: Made the helper context menu multiguild

--- src/redis/schemas/GuildPreferences.ts
+++ src/redis/schemas/GuildPreferences.ts
@@ -8,44 +8,7 @@ import type { IGuildPreferences } from "@/mongo";
 
 type ICachedGuildPreferences = Omit<IGuildPreferences, "guildId"> & Entity;
 
-const schema = new Schema("GuildPreferences", {
-	modlogChannelId: { type: "string" },
-	botlogChannelId: { type: "string" },
-	welcomeChannelId: { type: "string" },
-	repEnabled: { type: "boolean" },
-	repDisabledChannelIds: { type: "string[]" },
-	igHelperRoleId: { type: "string" },
-	alHelperRoleId: { type: "string" },
-	// igHelperRoles: {
-	// 	roleId: string;
-	// 	channelId: string;
-	// }[];
-	// alHelperRoles: {
-	// 	roleId: string;
-	// 	channelId: string;
-	// }[];
-	adminRoleId: { type: "string" },
-	moderatorRoleId: { type: "string" },
-	chatModRoleId: { type: "string" },
-	banAppealFormLink: { type: "string" },
-	keyword: { type: "string", path: "$.keywords[*]" },
-	response: { type: "string", path: "$.keywords[*]" },
-	behaviorlogChannelId: { type: "string" },
-	warnlogChannelId: { type: "string" },
-	modmailChannelId: { type: "string" },
-	actionRequiredChannelId: { type: "string" },
-	modFeedbackChannelId: { type: "string" },
-	confessionsChannelId: { type: "string" },
-	confessionApprovalChannelId: { type: "string" },
-	countingChannelId: { type: "string" },
-	hotmResultsChannelId: { type: "string" },
-	studySessionChannelId: { type: "string" },
-	chatmodApplicationsChannelId: { type: "string" },
-	colorRolesRoleId: { type: "string" },
-	emoji: { type: "string", path: "$.colorRoles[*]" },
-	label: { type: "string", path: "$.colorRoles[*]" },
-	id: { type: "string", path: "$.colorRoles[*]" },
-});
+const schema = new Schema("GuildPreferences", {});
 
 export class GuildPreferencesRepository extends Repository {
 	constructor(clientOrConnection: RedisConnection) {
