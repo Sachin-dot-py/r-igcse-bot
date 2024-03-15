0947723a6a04c3852e76453580991e8e202102d9
11xdeveloper <neighborhood10xdeveloper@gmail.com>
2024-03-15 17:17:07 +0500

    feat: Updated redis guild preferences schema

--- src/redis/schemas/GuildPreferences.ts
+++ src/redis/schemas/GuildPreferences.ts
@@ -8,7 +8,32 @@ import type { IGuildPreferences } from "@/mongo";
 
 type ICachedGuildPreferences = Omit<IGuildPreferences, "guildId"> & Entity;
 
-const schema = new Schema("GuildPreferences", {});
+const schema = new Schema("GuildPreferences", {
+	repEnabled: { type: "boolean" },
+	repDisabledChannelIds: { type: "string[]" },
+	modlogChannelId: { type: "string" },
+	behaviorlogChannelId: { type: "string" },
+	warnlogChannelId: { type: "string" },
+	botlogChannelId: { type: "string" },
+	botNewsChannelId: { type: "string" },
+	actionRequiredChannelId: { type: "string" },
+	welcomeChannelId: { type: "string" },
+	confessionsChannelId: { type: "string" },
+	confessionApprovalChannelId: { type: "string" },
+	countingChannelId: { type: "string" },
+	hotmResultsChannelId: { type: "string" },
+	studySessionChannelId: { type: "string" },
+	modmailChannelId: { type: "string" },
+	dmThreadsChannelId: { type: "string" },
+	banAppealFormLink: { type: "string" },
+	keywords: { type: "string[]" },
+	emoji: { type: "string", path: "$.colorRoles[*]" },
+	label: { type: "string", path: "$.colorRoles[*]" },
+	id: { type: "string", path: "$.colorRoles[*]" },
+	requirementRoleId: { type: "string", path: "$.colorRoles[*]" },
+	roleId: { type: "string", path: "$.helperRoles[*]" },
+	channelId: { type: "string", path: "$.helperRoles[*]" },
+});
 
 export class GuildPreferencesRepository extends Repository {
 	constructor(clientOrConnection: RedisConnection) {
