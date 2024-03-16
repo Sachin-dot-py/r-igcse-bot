0a91d59a740d9d05463e6b599677bba5cd733b90
11xdeveloper <neighborhood10xdeveloper@gmail.com>
2024-03-17 01:04:46 +0500

    feat: Automod timeouts accounted for in Punishment DB

---
+++ src/events/AutoModerationActionExecution.ts
@@ -0,0 +1,98 @@
+import { Punishment } from "@/mongo";
+import { GuildPreferencesCache } from "@/redis";
+import Logger from "@/utils/Logger";
+import {
+	AutoModerationActionExecution,
+	AutoModerationActionType,
+	Colors,
+	EmbedBuilder,
+	Events,
+} from "discord.js";
+import humanizeDuration from "humanize-duration";
+import type { DiscordClient } from "../registry/DiscordClient";
+import BaseEvent from "../registry/Structure/BaseEvent";
+
+export default class ErrorEvent extends BaseEvent {
+	constructor() {
+		super(Events.AutoModerationActionExecution);
+	}
+
+	async execute(
+		client: DiscordClient<true>,
+		autoModerationActionExecution: AutoModerationActionExecution,
+	) {
+		if (
+			!(
+				autoModerationActionExecution.action.type ===
+				AutoModerationActionType.Timeout
+			) ||
+			!autoModerationActionExecution.user
+		)
+			return;
+
+		const duration =
+			autoModerationActionExecution.action.metadata.durationSeconds ?? 0;
+
+		const reason =
+			autoModerationActionExecution.action.metadata.customMessage || null;
+
+		const durationString = humanizeDuration(duration * 1000);
+
+		const latestPunishment = await Punishment.findOne()
+			.sort({ createdAt: -1 })
+			.exec();
+
+		const caseNumber = (latestPunishment?.caseId ?? 0) + 1;
+
+		const guildPreferences = await GuildPreferencesCache.get(
+			autoModerationActionExecution.guild.id,
+		);
+
+		if (!guildPreferences) return;
+
+		await Punishment.create({
+			guildId: autoModerationActionExecution.guild.id,
+			actionAgainst: autoModerationActionExecution.userId,
+			actionBy: "Auto Mod",
+			action: "Timeout",
+			caseId: caseNumber,
+			duration,
+			reason,
+			points: duration >= 604800 ? 4 : duration >= 21600 ? 3 : 2,
+		});
+
+		const modEmbed = new EmbedBuilder()
+			.setTitle(`User Timed Out | Case #${caseNumber} | Auto Mod`)
+			.setDescription(reason)
+			.setColor(Colors.Red)
+			.setAuthor({
+				name: autoModerationActionExecution.user.displayName,
+				iconURL: autoModerationActionExecution.user.displayAvatarURL(),
+			})
+			.addFields([
+				{
+					name: "User ID",
+					value: autoModerationActionExecution.user.id,
+					inline: true,
+				},
+				{
+					name: "Duration",
+					value: durationString,
+					inline: true,
+				},
+				{
+					name: "Reason",
+					value: reason || "No reason provided.",
+					inline: false,
+				},
+			]);
+
+		await Logger.channel(
+			autoModerationActionExecution.guild,
+			guildPreferences.modlogChannelId,
+			{
+				embeds: [modEmbed],
+			},
+		);
+	}
+}
