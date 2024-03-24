751cb7b1fd649f2a2b238183374491a084beba8f
dotVansh <74044153+dotVansh@users.noreply.github.com>
2024-03-24 10:00:05 +0530

    Update Helper pings to not show "Created helper ping" (#41)

--- src/commands/menus/Helper.ts
+++ src/commands/menus/Helper.ts
@@ -97,10 +97,6 @@ export default class HelperMenu extends BaseCommand {
 			cancelButton
 		);
 
-		interaction.reply({
-			content: "Created helper ping"
-		});
-
 		const pingMessage = await interaction.channel.send({
 			embeds: [embed],
 			components: [row]
