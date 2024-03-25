fd6ff0bc7a603affde945a1dbe15773f659315ae
Sachin Ramanathan <61686882+Sachin-dot-py@users.noreply.github.com>
2024-03-25 16:20:20 -0700

    Fix Permissions for ReactionRoles

--- src/commands/moderation/roles/ReactionRoles.ts
+++ src/commands/moderation/roles/ReactionRoles.ts
@@ -37,6 +37,7 @@ export default class ReactionRolesCommand extends BaseCommand {
 								.setRequired(true)
 						)
 				)
+				.setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
 		);
 	}
 
