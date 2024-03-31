260599c984186e5c7793212e2c173e9182d879c7
dotVansh <74044153+dotVansh@users.noreply.github.com>
2024-03-31 17:30:41 +0530

    Fix unrounded durations messing up the time

--- src/commands/moderation/timeout/Timeout.ts
+++ src/commands/moderation/timeout/Timeout.ts
@@ -209,7 +209,7 @@ export default class TimeoutCommand extends BaseCommand {
 		}
 
 		interaction.reply({ content: "alrighty, timed them out", ephemeral: true });
-		const time = Math.floor(Date.now() / 1000) + duration
+		const time = Math.round(Math.floor(Date.now() / 1000) + duration)
 		interaction.channel.send(
 			`${user.username} has been timed out for *${reason}* until <t:${time}:f>. (<t:${time}:R>)`
 		);
