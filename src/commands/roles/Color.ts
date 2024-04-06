5b746d143e0b19aaa418a122e7d0e3b082d32e23
Chirag L <me@chirag.dev>
2024-04-06 23:42:22 +0530

    create a reusable paginator (#71)

--- src/commands/roles/Color.ts
+++ src/commands/roles/Color.ts
@@ -93,7 +93,9 @@ export default class ColorRolesCommand extends BaseCommand {
 			const role = interaction.guild.roles.cache.get(i.values[0]);
 
 			if (!role) {
-				await i.member.roles.remove(colorRoles.map((colorRole) => colorRole.roleId)).catch(() => { });
+				await i.member.roles
+					.remove(colorRoles.map((colorRole) => colorRole.roleId))
+					.catch(() => {});
 				interaction.followUp({
 					content: "All color roles have been removed from you.",
 					ephemeral: true
@@ -109,7 +111,7 @@ export default class ColorRolesCommand extends BaseCommand {
 				} else {
 					await i.member.roles
 						.remove(colorRoles.map((colorRole) => colorRole.roleId))
-						.catch(() => { });
+						.catch(() => {});
 					await i.member.roles.add(role);
 				}
 
