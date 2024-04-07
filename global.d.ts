2c77255aa2720cf9ae896a46b2f424df85f3b374
chirag350 <me@chirag.dev>
2024-04-07 18:42:59 +0530

    remove unused env variables

--- global.d.ts
+++ global.d.ts
@@ -5,10 +5,6 @@ declare module "bun" {
 		REDIS_URL: string;
 		MAIN_GUILD_ID: string;
 		ERROR_LOGS_CHANNEL_ID: string;
-		CHAT_MOD_APPS_CHANNEL_ID: string;
-		MOD_FEEDBACK_CHANNEL_ID: string;
-		DEV_FEEDBACK_CHANNEL_ID: string;
-		DEBATE_APPS_CHANNEL_ID: string;
 		[key: string]: string | undefined;
 	}
 }
