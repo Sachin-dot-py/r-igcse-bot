da45507218a51ac37581c3b253b9d23f0064cfc4
Muhammad <iqat476@gmail.com>
2024-04-14 16:02:10 +0400

    fix: /slowmode: merged subcommands into one, use 0s instead to remove slowmode

--- src/commands/moderation/Slowmode.ts
+++ src/commands/moderation/Slowmode.ts
@@ -12,33 +12,17 @@ export default class SlowmodeCommand extends BaseCommand {
                 .setName("slowmode")
                 .setDescription("Set the slowmode time (for mods)")
                 .setDMPermission(false)
-                .addSubcommand((subcommand) =>
-                    subcommand
-                        .setName("add")
-                        .setDescription("Add a slowmode")
-                        .addStringOption((option) =>
-                            option
-                                .setName("time")
-                                .setDescription("Slowmode time")
-                                .setRequired(true)
-                        )
-                        .addChannelOption((option) =>
-                            option
-                                .setName("channel")
-                                .setDescription("The channel to add the slowmode to")
-                                .setRequired(false)
-                        )
+                .addStringOption((option) =>
+                    option
+                        .setName("time")
+                        .setDescription("Slowmode time")
+                        .setRequired(true)
                 )
-                .addSubcommand((subcommand) =>
-                    subcommand
-                        .setName("remove")
-                        .setDescription("Removes the current slowmode")
-                        .addChannelOption((option) =>
-                            option
-                                .setName("channel")
-                                .setDescription("Channel to remove the slowmode from")
-                                .setRequired(false)
-                        )
+                .addChannelOption((option) =>
+                    option
+                        .setName("channel")
+                        .setDescription("The channel to add the slowmode to")
+                        .setRequired(false)
                 )
                 .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
         );
@@ -48,7 +32,7 @@ export default class SlowmodeCommand extends BaseCommand {
         client: DiscordClient<true>,
         interaction: DiscordChatInputCommandInteraction<"cached">
     ) {
-        const timeString = interaction.options.getString("time") ?? "0s";
+        const timeString = interaction.options.getString("time", true) ?? "0s";
         const channel = interaction.options.getChannel("channel", false) ?? interaction.channel;
 
         const time = parse(timeString, "second") ?? 0
@@ -61,30 +45,19 @@ export default class SlowmodeCommand extends BaseCommand {
             return;
         }
 
-        switch (interaction.options.getSubcommand()) {
-            case "add":
-                if (time > 21600 || time < 1) {
-                    interaction.reply({
-                        content: "Enter a valid time between 6 hours and 1 second",
-                        ephemeral: true
-                    });
-                    return;
-                }
-
-                await channel.setRateLimitPerUser(time);
-
-                interaction.reply({
-                    content: `Added a slowmode of ${timeString} to ${channel}`,
-                    ephemeral: true
-                });
-                break;
-            case "remove":
-                await channel.setRateLimitPerUser(0);
-                interaction.reply({
-                    content: `Removed slowmode from ${channel}`,
-                    ephemeral: true
-                })
-                break;
+        if (time > 21600 || time < 0) {
+            interaction.reply({
+                content: "Enter a valid time between 6 hours and 0 seconds",
+                ephemeral: true
+            });
+            return;
         }
+
+        await channel.setRateLimitPerUser(time);
+
+        interaction.reply({
+            content: `Added a slowmode of ${timeString} to ${channel}`,
+            ephemeral: true
+        });
     }
 }
