24a32f0ab6de65c53f5e8ece41261233d0b90991
Muhammad <iqat476@gmail.com>
2024-05-13 12:33:29 +0400

    feat: schedule messages/embeds

---
+++ src/mongo/schemas/ScheduledMessage.ts
@@ -0,0 +1,21 @@
+import { Schema, model as createModel } from "mongoose";
+import { type MessageCreateOptions } from "discord.js";
+
+export interface IScheduledMessage {
+	guildId: string;
+	channelId: string;
+	message: MessageCreateOptions;
+	scheduleTime: string;
+}
+
+const schema = new Schema<IScheduledMessage>({
+	guildId: { type: String, required: true, unique: false },
+	channelId: { type: String, required: true, unique: false },
+	message: { type: Object, required: true, unique: false },
+	scheduleTime: { type: String, required: true, unique: false }
+});
+
+export const ScheduledMessage = createModel<IScheduledMessage>(
+	"ScheduledMessage",
+	schema
+);
