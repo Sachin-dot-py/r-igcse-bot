b0d384b65249c2e3cc0ef3c100c52c1165889034
Ateeb Sohail <challengebudies9@gmail.com>
2024-07-14 03:18:36 +0500

    Final

--- src/events/ClientReady.ts
+++ src/events/ClientReady.ts
@@ -56,22 +56,23 @@ export default class ClientReadyEvent extends BaseEvent {
 				embeds: [readyEmbed],
 			});
 			createTask(async ()=>{
-				const channel = await mainGuild.channels.fetch(process.env.RESULT_REMINDER_CHANNEL_ID)
+				const channel = await mainGuild.channels.fetch(process.env.RESULT_REMINDER_CHANNEL_ID);
 				if(channel && channel.isTextBased()) {
 					const today = new Date();
 					const targetDate = new Date(2024, 7, 13, 5, 0, 0);
 					const diffInMs = targetDate.getTime() - today.getTime();
+					const secondsLeft = Math.floor(diffInMs / 1000);
 					const hoursLeft = Math.floor(diffInMs / (1000 * 60 * 60));
 					const daysLeft = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 					const embed = new EmbedBuilder()
 						.setColor(Colors.Blurple)
 						.setTitle('Time till result day!')
 						.setDescription(
-							`Result day <t:1723525200:R>\n\nThere are exactly **${daysLeft}** days left till result day.\nThere are exactly **${hoursLeft}** hours left till result time.`
-						)
-					channel.send({embeds: [embed]})
-				}
-			}, 3600000)
+							`Result day <t:1723525200:R>\n\nThere are exactly **${daysLeft}** days left till result day.\nThere are exactly **${hoursLeft}** hours left till result time.\nThere are exactly **${secondsLeft}** seconds left till result day.	`
+						);
+					channel.send({embeds: [embed]});
+				};
+			}, 3600000);
 		}
 
 		const goStudyCommand = client.commands.get("gostudy") as
