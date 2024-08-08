7bea91040104666567814f71498ad5d6ed6799fe
dotVansh <74044153+dotVansh@users.noreply.github.com>
2024-08-08 19:42:00 +0530

    Old Pins Fix

---
+++ src/mongo/schemas/OldPinsThread.ts
@@ -0,0 +1,13 @@
+import { Schema, model as createModel } from "mongoose";
+
+export interface IOldPinsThread {
+	channelId: string;
+	oldPinsThreadId: string;
+}
+
+const schema = new Schema<IOldPinsThread>({
+	channelId: { type: String, required: true, unique: false },
+	oldPinsThreadId: { type: String, required: true, unique: false },
+});
+
+export const OldPinsThread = createModel<IOldPinsThread>("OldPinsThread", schema);
