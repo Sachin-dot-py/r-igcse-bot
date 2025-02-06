dbb3ab692f535187e0fff04618080513ff1e1b7d
Rishab Sharma <77726912+donnutdev@users.noreply.github.com>
2025-02-06 23:48:14 +0530

    Reaction roles fix (#168)
    * switched create dm to dm_user command
    
    * added check for message.system
    
    * changed resource tag context menu name
    
    * mod stuff
    
    * mod stuff
    
    * mod stuff
    
    * new dockerfile
    
    * new dockerfile
    
    * hotm blacklist
    
    * hotm blacklist
    
    * fixed reaction roles
    
    * fixed reaction roles

--- src/events/MessageReactionRemove.ts
+++ src/events/MessageReactionRemove.ts
@@ -18,6 +18,8 @@ export default class MessageReactionRemoveEvent extends BaseEvent {
 
 		if (!reaction.message.guild) return;
 
+		if (reaction.partial) reaction = await reaction.fetch();
+
 		const res = await ReactionRole.findOne({
 			messageId: reaction.message.id,
 			emoji: reaction.emoji.toString(),
@@ -25,9 +27,9 @@ export default class MessageReactionRemoveEvent extends BaseEvent {
 
 		if (!res) return;
 
-		const member = await reaction.message.guild.members.fetch(user.id);
+		const member = await reaction.message.guild!.members.fetch(user.id);
 
-		const role = await reaction.message.guild.roles.fetch(res.roleId);
+		const role = await reaction.message.guild!.roles.fetch(res.roleId);
 
 		if (role) await member.roles.remove(role);
 	}
