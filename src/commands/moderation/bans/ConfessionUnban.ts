f44537b997580affeed14e2a56b482aa93546954
Iqat <iqat476@gmail.com>
2025-02-20 19:28:50 +0400

    feat: add case numbers

--- src/commands/moderation/bans/ConfessionUnban.ts
+++ src/commands/moderation/bans/ConfessionUnban.ts
@@ -1,121 +1,129 @@
-import { Punishment, ConfessionBan } from "@/mongo";
+import { ConfessionBan } from "@/mongo";
 import { GuildPreferencesCache } from "@/redis";
 import type { DiscordClient } from "@/registry/DiscordClient";
 import BaseCommand, {
-  type DiscordChatInputCommandInteraction,
+	type DiscordChatInputCommandInteraction,
 } from "@/registry/Structure/BaseCommand";
 import { logToChannel } from "@/utils/Logger";
-import sendDm from "@/utils/sendDm";
 import {
-  Colors,
-  EmbedBuilder,
-  PermissionFlagsBits,
-  SlashCommandBuilder,
+	Colors,
+	EmbedBuilder,
+	PermissionFlagsBits,
+	SlashCommandBuilder,
 } from "discord.js";
 
 interface IBannedData {
-  _id: string;
-  bannedUsers: {_id: string, userHash: string}[];
+	_id: string;
+	bannedUsers: { _id: string; userHash: string }[];
 }
 
 export default class ConfessionUnban extends BaseCommand {
-  constructor() {
-    super(
-      new SlashCommandBuilder()
-        .setName("confession_unban")
-        .setDescription("Unban a user from confessions (for mods)")
-        .addUserOption((option) =>
-          option.setName("user").setDescription("User that you want to unban").setRequired(true)
-        )
-        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-        .setDMPermission(false)
-    );
-  }
+	constructor() {
+		super(
+			new SlashCommandBuilder()
+				.setName("confession_unban")
+				.setDescription("Unban a user from confessions (for mods)")
+				.addUserOption((option) =>
+					option
+						.setName("user")
+						.setDescription("User that you want to unban")
+						.setRequired(true),
+				)
+				.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+				.setDMPermission(false),
+		);
+	}
 
-  async execute(
-    client: DiscordClient<true>,
-    interaction: DiscordChatInputCommandInteraction<"cached">
-  ) {
-    if (!interaction.channel || !interaction.channel.isTextBased()) return;
+	async execute(
+		client: DiscordClient<true>,
+		interaction: DiscordChatInputCommandInteraction<"cached">,
+	) {
+		if (!interaction.channel || !interaction.channel.isTextBased()) return;
 
-    const user = interaction.options.getUser("user", true);
+		const user = interaction.options.getUser("user", true);
 
-    await interaction.deferReply({
-      ephemeral: true,
-    });
-    
-    const guildPreferences = await GuildPreferencesCache.get(
-        interaction.guildId
-    );
+		await interaction.deferReply({
+			ephemeral: true,
+		});
 
-    const bannedQuery: IBannedData[] | null =
-        (await ConfessionBan.aggregate([
-          {
-            $match: {
-              guildId: interaction.guild.id
-            }
-          },
-          {
-            $group: {
-              _id: "$guildId",
-              bannedUsers: {
-                $push: {
-                  _id: "$_id",
-                  userHash: "$userHash"
-                }
-              }
-            }
-          }
-        ])) ?? null;
+		const guildPreferences = await GuildPreferencesCache.get(
+			interaction.guildId,
+		);
 
-    const bannedUsers = bannedQuery[0]?.bannedUsers || [];
+		const bannedQuery: IBannedData[] | null =
+			(await ConfessionBan.aggregate([
+				{
+					$match: {
+						guildId: interaction.guild.id,
+					},
+				},
+				{
+					$group: {
+						_id: "$guildId",
+						bannedUsers: {
+							$push: {
+								_id: "$_id",
+								userHash: "$userHash",
+							},
+						},
+					},
+				},
+			])) ?? null;
 
-    let idToUnban = null;
+		const bannedUsers = bannedQuery[0]?.bannedUsers || [];
 
-    for (const bannedUser of bannedUsers) {
-      if (await Bun.password.verify(interaction.user.id, bannedUser.userHash)) {
-        idToUnban = bannedUser._id;
-        break
-      }
-    }
+		let idToUnban = null;
 
-    if (idToUnban === null) {
-      interaction.editReply({
-        content: "This user isn't banned from confessions.",
-      });
-      return;
-    }
+		for (const bannedUser of bannedUsers) {
+			if (
+				await Bun.password.verify(
+					interaction.user.id,
+					bannedUser.userHash,
+				)
+			) {
+				idToUnban = bannedUser._id;
+				break;
+			}
+		}
 
-    await ConfessionBan.findByIdAndDelete(idToUnban);
+		if (idToUnban === null) {
+			interaction.editReply({
+				content: "This user isn't banned from confessions.",
+			});
+			return;
+		}
 
-    if (guildPreferences?.modlogChannelId) {
-      const modEmbed = new EmbedBuilder()
-          .setTitle(`Confession Unban`)
-          .setColor(Colors.Red)
-          .addFields([
-            {
-              name: "User",
-              value: `${user.tag} (${user.id})`,
-              inline: false,
-            },
-            {
-              name: "Moderator",
-              value: `${interaction.user.tag} (${interaction.user.id})`,
-              inline: false,
-            },
-          ])
-          .setTimestamp();
+		await ConfessionBan.findByIdAndDelete(idToUnban);
 
-      logToChannel(interaction.guild, guildPreferences.modlogChannelId, {
-        embeds: [modEmbed],
-      });
-    }
+		if (guildPreferences?.modlogChannelId) {
+			const modEmbed = new EmbedBuilder()
+				.setTitle("Confession Unban")
+				.setColor(Colors.Red)
+				.addFields([
+					{
+						name: "User",
+						value: `${user.tag} (${user.id})`,
+						inline: false,
+					},
+					{
+						name: "Moderator",
+						value: `${interaction.user.tag} (${interaction.user.id})`,
+						inline: false,
+					},
+				])
+				.setTimestamp();
 
-    interaction.editReply({
-      content: "https://tenor.com/view/spooky-season-skeleton-dance-dancing-skeleton-gif-15410799",
-    });
-    interaction.channel.send({
-      content: `${user.username} has been unbanned from confessions.`,
-    });
-  }
+			logToChannel(interaction.guild, guildPreferences.modlogChannelId, {
+				embeds: [modEmbed],
+			});
+		}
+
+		interaction.editReply({
+			content:
+				"https://tenor.com/view/spooky-season-skeleton-dance-dancing-skeleton-gif-15410799",
+		});
+		interaction.channel.send({
+			content: `${user.username} has been unbanned from confessions.`,
+		});
+	}
 }
