0515db830d9d58d1c4ed8027f862af20a519dc10
juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2025-05-21 13:46:48 +0800

    Fixes & Rewrites
    - Fixed Automod Reasoning
    - Removed `remove_lockdown`
    - Made Lockdown with 2 subcommands called create & delete
    - Fixed Fetching Lockdowns

--- src/mongo/schemas/ChannelLockdown.ts
+++ src/mongo/schemas/ChannelLockdown.ts
@@ -1,17 +1,21 @@
 import { Schema, model as createModel } from "mongoose";
 
 export interface IChannelLockdown {
+	lockID: string;
 	guildId: string;
 	channelId: string;
 	startTimestamp: string;
 	endTimestamp: string;
+	locked: boolean;
 }
 
 const schema = new Schema<IChannelLockdown>({
+	lockID: { type: String, required: true, unique: true },
 	guildId: { type: String, required: true, unique: false },
 	channelId: { type: String, required: true, unique: true },
 	startTimestamp: { type: String, required: true, unique: false },
 	endTimestamp: { type: String, required: true, unique: false },
+	locked: { type: Boolean, required: true, unique: false },
 });
 
 export const ChannelLockdown = createModel<IChannelLockdown>(
