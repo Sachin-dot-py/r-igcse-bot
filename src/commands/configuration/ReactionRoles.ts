6dd60d1103a053cb60b205edcb40b84721fe831b
NithanZa <79699769+NithanZa@users.noreply.github.com>
2025-07-13 04:30:51 -0700

    import MessageFlags for remaining files (#200)

--- src/commands/configuration/ReactionRoles.ts
+++ src/commands/configuration/ReactionRoles.ts
@@ -4,7 +4,7 @@ import type { DiscordClient } from "@/registry/DiscordClient";
 import BaseCommand, {
 	type DiscordChatInputCommandInteraction,
 } from "@/registry/Structure/BaseCommand";
-import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { MessageFlags, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 
 export default class ReactionRolesCommand extends BaseCommand {
 	constructor() {
