009b9d57cdcd018bc73f59290d732990e0b2e5c4
Adam Al Shouli <adamalshouli05@gmail.com>
2025-12-29 19:43:24 +0300

    add optional deleted entry to ReputationData mongodb scheme

--- src/mongo/schemas/ReputationData.ts
+++ src/mongo/schemas/ReputationData.ts
@@ -1,21 +1,26 @@
 import { Schema, model as createModel } from "mongoose";
 
 export interface IReputationData {
-	reppedUser: string;
+    reppedUser: string;
     reppedBy?: string | null;
-	repNumber: number;
+    repNumber: number;
     when: Date;
     channelId: string;
-	guildId: string;
+    guildId: string;
+    deleted?: boolean | null;
 }
 
 const schema = new Schema<IReputationData>({
-	reppedUser: { type: String, required: true },
-	reppedBy: { type: String, required: false},
-	repNumber: { type: Number, required: true },
+    reppedUser: { type: String, required: true },
+    reppedBy: { type: String, required: false },
+    repNumber: { type: Number, required: true },
     when: { type: Date, required: true },
-	channelId: { type: String, required: true},
-	guildId: { type: String, required: true },
+    channelId: { type: String, required: true },
+    guildId: { type: String, required: true },
+    deleted: { type: Boolean, required: false },
 });
 
-export const ReputationData = createModel<IReputationData>("ReputationData", schema);
+export const ReputationData = createModel<IReputationData>(
+    "ReputationData",
+    schema
+);
