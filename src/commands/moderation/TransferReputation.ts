7d13a78d1dd00bfd704cf8381138be7702724bb1
Adam Al Shouli <adamalshouli05@gmail.com>
2025-12-30 13:01:01 +0300

    feat: Add transfer_rep command

---
+++ src/commands/moderation/TransferReputation.ts
@@ -0,0 +1,145 @@
+import { Reputation, ReputationData } from "@/mongo";
+import type { DiscordClient } from "@/registry/DiscordClient";
+import BaseCommand, {
+    type DiscordChatInputCommandInteraction,
+} from "@/registry/Structure/BaseCommand";
+import {
+    MessageFlags,
+    PermissionFlagsBits,
+    SlashCommandBuilder,
+} from "discord.js";
+
+export default class extends BaseCommand {
+    constructor() {
+        super(
+            new SlashCommandBuilder()
+                .setName("transfer_rep")
+                .setDescription(
+                    "Transfer a users reputation to another account (for mods)"
+                )
+                .setDMPermission(false)
+                .addUserOption((option) =>
+                    option
+                        .setName("sender")
+                        .setDescription("The rep sender")
+                        .setRequired(true)
+                )
+                .addUserOption((option) =>
+                    option
+                        .setName("recipient")
+                        .setDescription("The rep recipient")
+                        .setRequired(true)
+                )
+                .addIntegerOption((option) =>
+                    option
+                        .setName("amount")
+                        .setDescription(
+                            "The amount of rep to transfer (defaults to all)"
+                        )
+                        .setRequired(false)
+                )
+                .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+        );
+    }
+
+    async execute(
+        client: DiscordClient<true>,
+        interaction: DiscordChatInputCommandInteraction<"cached">
+    ) {
+        const sender = interaction.options.getUser("sender", true);
+        const recipient = interaction.options.getUser("recipient", true);
+
+        const currentSenderRepDocument = await Reputation.findOne({
+            guildId: interaction.guild.id,
+            userId: sender.id,
+        });
+
+        const currentRecipientDocument = await Reputation.findOne({
+            guildId: interaction.guild.id,
+            userId: recipient.id,
+        });
+
+        const currentSenderRep = currentSenderRepDocument?.rep ?? 0;
+        const currentRecipientRep = currentRecipientDocument?.rep ?? 0;
+
+        const amount =
+            interaction.options.getInteger("amount", false) ?? currentSenderRep;
+
+        if (currentSenderRep < 1) {
+            await interaction.reply({
+                content: "The sender does not have any rep 😭🙏",
+                flags: MessageFlags.Ephemeral,
+            });
+            return;
+        } else if (amount < 1) {
+            await interaction.reply({
+                content: "You must transfer at least 1 rep",
+                flags: MessageFlags.Ephemeral,
+            });
+            return;
+        } else if (currentSenderRep < amount) {
+            await interaction.reply({
+                content: `The sender (<@${sender.id}>) does not have enough rep (${currentSenderRep})`,
+                flags: MessageFlags.Ephemeral,
+            });
+            return;
+        }
+
+        const newSenderRep = currentSenderRep - amount;
+        const newRecipientRep = currentRecipientRep + amount;
+
+        await Reputation.updateOne(
+            {
+                guildId: interaction.guild.id,
+                userId: sender.id,
+            },
+            {
+                $set: { rep: newSenderRep },
+            },
+            {
+                upsert: true,
+            }
+        );
+
+        await Reputation.updateOne(
+            {
+                guildId: interaction.guild.id,
+                userId: recipient.id,
+            },
+            {
+                $set: { rep: newRecipientRep },
+            },
+            {
+                upsert: true,
+            }
+        );
+
+        const toBeTransferred = await ReputationData.find({
+            guildId: interaction.guildId,
+            reppedUser: sender.id,
+            deleted: { $ne: true },
+        })
+            .sort({ repNumber: -1, when: -1 })
+            .limit(amount);
+
+        if (toBeTransferred.length) {
+            const transfers = toBeTransferred.reverse().map((doc, i) => ({
+                updateOne: {
+                    filter: { _id: doc._id },
+                    update: {
+                        $set: {
+                            reppedUser: recipient.id,
+                            repNumber: currentRecipientRep + i + 1,
+                        },
+                    },
+                },
+            }));
+
+            await ReputationData.bulkWrite(transfers);
+        }
+
+        await interaction.reply({
+            content: `Transferred ${amount} rep from <@${sender.id}> to <@${recipient.id}>`,
+        });
+    }
+}
