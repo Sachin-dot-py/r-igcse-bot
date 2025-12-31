2bf71cbd5ae55db039416e856fb76bc6c70dce21
Iqat <iqat476@gmail.com>
2025-12-31 13:50:32 +0400

    fix: modal 45 character limit (#226)
    - fixed the 45 character limit by using a shorter label and putting the full question as the placeholder

--- src/mongo/schemas/GetRoleQnA.ts
+++ src/mongo/schemas/GetRoleQnA.ts
@@ -3,10 +3,12 @@ import { Schema, model as createModel } from "mongoose";
 export interface IGetRoleQnA {
 	question: string;
 	answers: string[];
+    label: string;
 }
 
 const schema = new Schema<IGetRoleQnA>({
 	question: { type: String, required: true },
+    label: { type: String, required: true },
 	answers: { type: [String], required: true },
 });
 
